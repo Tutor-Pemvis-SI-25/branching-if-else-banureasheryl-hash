b84c57f2e4fdb497d616adc53ac9543a834979af
banureasheryl-hash <banureasheryl@gmail.com>
2025-10-03 15:32:37 +0700

    PAIR 24
    SUDAH DIPERBAIKI

--- T00.java
+++ T00.java
@@ -31,16 +31,16 @@ public class T00 {
             }
         }
         if (a % 2 == 0 && b % 2 == 0) {
-            System.out.println(" Hasil penjumlahan:" + a + b);
+            System.out.println(" Hasil penjumlahan: " + a + b);
         } else {
             if (a % 2 != 0 && b % 2 != 0) {
-                System.out.println(" Hasil perkalian:" + a * b);
+                System.out.println(" Hasil perkalian: " + a * b);
             } else {
                 if (a % 2 != 0 && b % 2 == 0) {
-                    System.out.println("Berbeda jenis");
+                    System.out.println("Berbeda Jenis");
                 } else {
                     if (a % 2 == 0 && b % 2 != 0) {
-                        System.out.println("Berbeda jenis");
+                        System.out.println("Berbeda Jenis");
                     }
                 }
             }
